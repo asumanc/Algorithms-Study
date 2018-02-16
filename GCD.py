de50787aae62ffc81538095a18a588dd96343d0a
A.Rumeysa Nur Celik <arncelik@yahoo.com>
2018-02-16 08:19:35 -0600

    Add files via upload

---
+++ GCD.py
@@ -0,0 +1,26 @@
+def gcd (a,b)
+	if b==0:
+	return a:
+
+	else return g cd (b,a mod b):
+
+-
+-
+-
+-
+#Other way:
+
+def gcd (a,b)
+
+	if ((a%b)==0):
+		return b
+	else 
+		return gcd (b,a%b)
+		pass
+
+#Other Way
+def gcd(a,b)*lcm(a,b)=ab
+
+	int lcm (int a, int b);
+
+return (a*b)/GCD-recursion (a,b)
