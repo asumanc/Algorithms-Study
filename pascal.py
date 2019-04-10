01f98af147e6125ee955f609063bd802bf487463
Asuman R.N Celik <arncelik@gmail.com>
2019-04-09 21:00:52 -0500

    Add files via upload

---
+++ pascal.py
@@ -0,0 +1,16 @@
+def pascal(row, column):
+  if (column == 1):
+    return 1
+  if (row == column):
+    return 1
+  
+  left = pascal(row-1, column-1)
+  right = pascal(row-1, column)
+  return left + right
+
+#driver function
+n = input()
+for r in range(1,n):
+  for col in range(1, r + 1):
+    print(pascal(r, col), end='')
+  print("")
