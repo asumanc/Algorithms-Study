4b62c1a00affb0c8ebc28fddcb45ba5105231add
Asuman R.N Celik <arncelik@gmail.com>
2019-04-10 07:09:28 -0500

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -1,24 +1,3 @@
-def gcd (a,b)
-	if b==0:
-	return a:
-
-	else return gcd (b,a mod b):
-
--
--
--
--
-#Other way:
-
-def gcd (a,b)
-
-	if ((a%b)==0):
-		return b
-	else 
-		return gcd (b,a%b)
-		pass
-
-#Other Way
 def gcd(a,b)*lcm(a,b)=ab
 
 	int lcm (int a, int b);
