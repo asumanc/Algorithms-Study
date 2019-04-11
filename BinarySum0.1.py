b0a9cfadafef935c6cd2a246c324155c185108dd
Asuman R.N Celik <arncelik@gmail.com>
2019-04-11 00:24:19 -0500

    Add files via upload

---
+++ BinarySum0.1.py
@@ -0,0 +1,12 @@
+# get input
+num1 = input()
+num2 = input()
+# converts to int using built-in function int()
+num1 = int(num1, 2)
+num2 = int(num2, 2)
+# adds up integers
+sum = num1 + num2
+# converts binary 
+sum = bin(sum)
+
+print(sum)
