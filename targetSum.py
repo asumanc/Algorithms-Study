6202a44d085e102f78ba28f115814d2b8cf2f4c2
Asuman R.N Celik <arncelik@gmail.com>
2019-04-16 00:55:27 -0500

    Add targetSum.py

---
+++ targetSum.py
@@ -0,0 +1,19 @@
+def targetSum(target, num):
+    left, right = 0, len(num) -1
+
+    while left < right:
+        temp = num[left] + num[right]
+
+        if temp > target :
+            right -= 1
+
+        elif temp < target:
+            left += 1
+
+        elif temp == target:
+            return right + 1, left + 1
+
+
+num = [int(i) for i in input().split()]
+target = int(input())
+print(targetSum(target, num))
