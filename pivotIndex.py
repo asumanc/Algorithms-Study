edabe7a764a3e087a76d3505f2c46ef663b8d76a
Asuman R.N Celik <arncelik@gmail.com>
2019-04-30 10:21:57 -0500

    Update pivotIndex.py

--- pivotIndex.py
+++ pivotIndex.py
@@ -2,7 +2,7 @@ def pivotIndex(self, nums):
     total = sum(nums)
     currentSum = 0
     for i in range(len(nums)):
-        if (currentSumsum << 1) == total - nums[i]:
+        if (currentSum << 1) == total - nums[i]:
             return i
         currentSum += nums[i]
     return -1
